5e188d92fcb6c673fbef95a38e5a39d8626ab5f2
Lowell Mower & Joseph Fields <pair+lowellmower.sevennote@devbootcamp.com>
2015-06-15 10:51:21 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Dog.create name: "Charlie", breed: "Puggle", age: 16
+Dog.create name: "Bill", breed: "Picaneeze", age: 1
+Dog.create name: "Cookie", breed: "Shizu", age: 7
+Dog.create name: "Pepperoni", breed: "Lab", age: 2
+Dog.create name: "Spot", breed: "Pug", age: 5
